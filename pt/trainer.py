c6d8d3c2405b110628eaedc2f9b757e5c92bf365
Tim Holdsworth <tholdsworth@sandiego.edu>
2023-05-09 16:21:40 -0700

    use tf data

--- pt/trainer.py
+++ pt/trainer.py
@@ -8,9 +8,11 @@ from models import ConvModelMNIST
 # Create tf data objects for MNIST 
 images, labels = download_qmnist()
 (x_train, y_train), (x_val, y_val), (x_holdout, y_holdout) = prep_data(images, labels)
-# train_dataset   = tf.data.Dataset.from_tensor_slices((x_train, y_train))
-# val_dataset     = tf.data.Dataset.from_tensor_slices((x_val, y_val))
-# holdout_dataset = tf.data.Dataset.from_tensor_slices((x_holdout, y_holdout))
+batch_size = 32
+train_dataset   = tf.data.Dataset.from_tensor_slices((x_train, y_train))    .batch(batch_size)
+val_dataset     = tf.data.Dataset.from_tensor_slices((x_val, y_val))        .batch(batch_size)
+holdout_dataset = tf.data.Dataset.from_tensor_slices((x_holdout, y_holdout)).batch(batch_size)
+    
 
 # Create the model
 model = ConvModelMNIST()
@@ -20,11 +22,7 @@ model.compile(
         metrics  = ['accuracy']
     )
 
-history = model.fit(x_train, y_train, 
+history = model.fit(train_dataset, 
                     epochs=1, 
-                    validation_data=(x_val, y_val), 
+                    validation_data=val_dataset, 
                     verbose=1)
-# history = model.fit(train_dataset, 
-#                     epochs=1, 
-#                     validation_data=val_dataset, 
-#                     verbose=1)
